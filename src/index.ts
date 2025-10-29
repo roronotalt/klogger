e32a9a23bfb3f1120a7dd34d75c8ecfb62009772
roronotalt <183158846+roronotalt@users.noreply.github.com>
2025-10-29 16:56:10 -0400

    using native dir

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,28 @@
 const default_levels = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    http: 3,
-    verbose: 4,
-    debug: 5,
-    silly: 6,
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    http: 4,
+    verbose: 5,
+    debug: 6,
+    silly: 7,
 } as const;
 
 export type KLoggerLevel = keyof typeof default_levels;
 
 const default_console_colors = {
-    error: "\x1b[31m", // red
+    fatal: "\x1b[31m", // red
+    error: "\x1b[91m", // light red
     warn: "\x1b[33m", // yellow
     info: "\x1b[36m", // cyan
-    http: "\x1b[35m", // magenta
+    http: "\x1b[92m", // light green
     verbose: "\x1b[37m", // white
     debug: "\x1b[90m", // gray
     silly: "\x1b[95m", // bright magenta
 };
 
 const RESET = "\x1b[0m";
-
-const safe_stringify = (obj: any, max_depth: number = 6): string => {
-    const seen = new WeakSet();
-
-    const helper = (value: any, depth: number): any => {
-        if (depth > max_depth) return "[MaxDepth]";
-        if (typeof value === "object" && value !== null) {
-            if (seen.has(value)) return "[Circular]";
-            seen.add(value);
-            if (Array.isArray(value))
-                return value.map((v) => helper(v, depth + 1));
-            const out: Record<string, any> = {};
-            for (const [k, v] of Object.entries(value))
-                out[k] = helper(v, depth + 1);
-            return out;
-        }
-        return value;
-    };
-
-    return JSON.stringify(helper(obj, 0));
-};
 
 export class KLogger {
     private level: KLoggerLevel;
@@ -63,25 +44,17 @@ export class KLogger {
                 default_levels[this.level as keyof typeof default_levels]
             ) {
                 (this as any)[level] = (...args: any[]) => {
-                    const prefix =
-                        this.service_name.toUpperCase() +
-                        ` ${new Date().toISOString()}`;
-
-                    const sanitize =
-                        typeof args[args.length - 1] === "boolean"
-                            ? (args.pop() as boolean)
-                            : false;
+                    const prefix = `${
+                        default_console_colors[
+                            level as keyof typeof default_console_colors
+                        ]
+                    }[${this.service_name.toUpperCase()}]${RESET} ${new Date().toISOString()}]`;
 
                     args.forEach((message) => {
-                        console.log(
-                            `${
-                                default_console_colors[
-                                    level as keyof typeof default_console_colors
-                                ]
-                            }[${prefix}]${RESET} ${
-                                sanitize ? safe_stringify(message) : message
-                            }`
-                        );
+                        typeof message === "object"
+                            ? (console.log(prefix),
+                              console.dir(message, { depth: 6 }))
+                            : console.log(`${prefix} ${message}`);
                     });
                 };
             }
@@ -118,8 +91,9 @@ export class KLogger {
         });
     }
 
-    public info(..._args: any[]): void {}
+    public fatal(..._args: any[]): void {}
     public error(..._args: any[]): void {}
+    public info(..._args: any[]): void {}
     public warn(..._args: any[]): void {}
     public verbose(..._args: any[]): void {}
     public debug(..._args: any[]): void {}
